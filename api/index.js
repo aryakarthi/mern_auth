857d654672e74f8fd99a9d2a9f7bdbd25018850d
aryakarthi <aryaperumal.m@gmail.com>
2023-12-25 22:42:09 +0530

    Connected to db.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
-import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
+
+// p267mpkwgtN9OCVS
